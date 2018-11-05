eb529110474193a5b278d984117f27bdb38e80a5
Blagovest Anev <blagovest.anev@endava.com>
2018-11-05 16:21:46 +0200

    swaggerImportIssue#3 swagger documentation added

---
+++ src/main/java/com/endava/demo/config/SwaggerConfig.java
@@ -0,0 +1,46 @@
+package com.endava.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * When the application is up and running, open "http://localhost:8085/program/swagger-ui.html"
+ * to access Swagger UI at your local environment.
+ */
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    private final static String API_BASE_PACKAGE = "com.endava.demo.controller";
+
+    @Bean
+    public Docket userApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(metaData())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(API_BASE_PACKAGE))
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    //API base information
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot REST API")
+                .description("Spring Boot REST API for Testing Project")
+                .version("v1.0")
+                .contact(new Contact("Blagovest Anev", "https://www.linkedin.com/in/blagovestanev/", "blagovestanev@gmail.com"))
+                .build();
+    }
+}
--- src/main/java/com/endava/demo/controller/UserController.java
+++ src/main/java/com/endava/demo/controller/UserController.java
@@ -2,6 +2,10 @@ package com.endava.demo.controller;
 
 import com.endava.demo.dto.UserDto;
 import com.endava.demo.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -18,20 +22,37 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
+@Api(description = "Contains operations for operations with the user-data.")
 public class UserController {
 
     private final UserService userService;
 
+    @ApiOperation(value = "Updates \"user\" information based on \"id\",\"login name\" and \"password\". If such don't exist, the app will create a new one.")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Returns \"OK\" response when successful update/create operation has been performed."),
+                           @ApiResponse(code = 401, message = "You are not authorized to view the resource."),
+                           @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden."),
+                           @ApiResponse(code = 404, message = "The resource you were trying to reach is not found.")})
     @PutMapping(consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity createOrUpdateUser(@Valid @RequestBody UserDto userDto) {
         return ResponseEntity.ok(this.userService.createOrUpdate(userDto));
     }
 
+    @ApiOperation(value = "Return information for \"user\", based on \"id\".")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Returns user information, based on \"id\" with OK response."),
+                           @ApiResponse(code = 204, message = "Returns \"No content\" message, when user with this \"id\" is not found."),
+                           @ApiResponse(code = 401, message = "You are not authorized to view the resource."),
+                           @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+                           @ApiResponse(code = 500, message = "Returns information that \"user\" with this id don't exist in database.")})
     @GetMapping(value = "/find/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity findUser(@PathVariable Long id) {
         return this.userService.find(id).map(ResponseEntity::ok).orElse(ResponseEntity.noContent().build());
     }
 
+    @ApiOperation(value = "Deletes \"user\", based on \"id\".")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Returns \"OK\" response when the \"user\" has been found and deleted."),
+                           @ApiResponse(code = 401, message = "You are not authorized to view the resource."),
+                           @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden."),
+                           @ApiResponse(code = 500, message = "Returns information that \"user\" with this id don't exist in database.")})
     @DeleteMapping(value = "/delete/{id}")
     public ResponseEntity deleteUser(@PathVariable Long id) {
         this.userService.delete(id);
